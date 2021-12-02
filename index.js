2cb30df0de2c54f3fcdb9b0978c24e4fc2d042dc
glanctot <gregory.lanctot28@gmail.com>
2021-12-02 15:59:04 -0600

    npm added

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-console.log('hello world');
+const generatePage = () => {
+    return `
+    # ${projectName}
+    ### Description
+    this is similar to the about me content from the module
+    ## Table of Contents
+    1. [Installation](#Installation)
+    2. [Usage](#Usage)
+    3. [License](#License)
+    4. [Contributing](#Contributing)
+    5. [Tests](#Tests)
+    6. [Questions](#Questions)
+    ### Usage
+    `
+}
+console.log(generatePage());
 
 // enter title for project, displayed as title for readme
 // enter description, installation instructions, usage info, contribution guidelines and test instructions then this info is added to corresponding sections
 // choose a license, badge added to top of readme and notice is added to section of license
 // enter github username and email then added to questions section
 // click on links in table of contents then taken to corresponding section of readme
+
+// installation: what steps are required to install your project? Provide step by step instructions on how to get running
+// usage: provide instructions and examples for use
+// license: explains which license the application is covered under
+// contributing: list any collaborators
+// tests: write tests for your application
+// questions: github username and email added to questions
