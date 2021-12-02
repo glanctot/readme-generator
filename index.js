908992d4ec049010f998273a4a4e594d8e35c114
glanctot <gregory.lanctot28@gmail.com>
2021-12-02 10:12:49 -0600

    push before creating branches

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log('hello world');
+
+// enter title for project, displayed as title for readme
+// enter description, installation instructions, usage info, contribution guidelines and test instructions then this info is added to corresponding sections
+// choose a license, badge added to top of readme and notice is added to section of license
+// enter github username and email then added to questions section
+// click on links in table of contents then taken to corresponding section of readme
